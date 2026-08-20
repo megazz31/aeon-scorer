399e85a8b9301c0decf512946f677ad9c44c555f
megazz31 <dandria.alan@gmail.com>
2026-08-20 20:38:16 +0200

    Wrap Aeon in the persistent cloud workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CloudWorkspace from './CloudWorkspace.jsx'
 import './styles.css'
 import './readability.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><CloudWorkspace><App /></CloudWorkspace></React.StrictMode>
 )
