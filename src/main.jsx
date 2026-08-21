44c5845cdb728f598a67d6a446378b1dbbcda1b9
megazz31 <dandria.alan@gmail.com>
2026-08-21 10:53:25 +0200

    Route public precon library from app entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CloudWorkspace from './CloudWorkspace.jsx'
+import PublicDecksPage from './PublicDecksPage.jsx'
 import './styles.css'
 import './readability.css'
+import './publicDecks.css'
+
+const publicDeckRoute=window.location.pathname==='/decklists-publiques'||window.location.pathname.startsWith('/decklists-publiques/')
+const publicLabel=localStorage.getItem('aeon-lang')==='fr'?'Decklists publiques':'Public decks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><CloudWorkspace><App /></CloudWorkspace></React.StrictMode>
+  <React.StrictMode>
+    {publicDeckRoute?<PublicDecksPage/>:<><CloudWorkspace><App /></CloudWorkspace><a className="publicLibraryShortcut" href="/decklists-publiques"><span>{publicLabel}</span></a></>}
+  </React.StrictMode>
 )
