6988de332348db1ec18cf5f37ddbf26876729e4c
megazz31 <dandria.alan@gmail.com>
2026-08-21 17:09:31 +0200

    Route Rule 0 shares and Pod Match pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CloudWorkspace from './CloudWorkspace.jsx'
 import PublicDecksPage from './PublicDecksPage.jsx'
+import { PodMatchPage,SharedAnalysisPage } from './ProductPages.jsx'
 import './styles.css'
 import './readability.css'
 import './publicDecks.css'
 
-const publicDeckRoute=window.location.pathname==='/decklists-publiques'||window.location.pathname.startsWith('/decklists-publiques/')
+const path=window.location.pathname
+const publicDeckRoute=path==='/decklists-publiques'||path.startsWith('/decklists-publiques/')
+const shareRoute=path.startsWith('/a/')
+const podRoute=path==='/pod'
 const publicLabel=localStorage.getItem('aeon-lang')==='fr'?'Decklists publiques':'Public decks'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {publicDeckRoute?<PublicDecksPage/>:<><CloudWorkspace><App /></CloudWorkspace><a className="publicLibraryShortcut" href="/decklists-publiques"><span>{publicLabel}</span></a></>}
-  </React.StrictMode>
-)
+let page
+if(publicDeckRoute)page=<PublicDecksPage/>
+else if(shareRoute)page=<SharedAnalysisPage/>
+else if(podRoute)page=<PodMatchPage/>
+else page=<><CloudWorkspace><App /></CloudWorkspace><a className="publicLibraryShortcut" href="/decklists-publiques"><span>{publicLabel}</span></a></>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>{page}</React.StrictMode>)
