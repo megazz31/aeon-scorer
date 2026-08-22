4230d98825068f20d1e368f517665164021a2828
megazz31 <dandria.alan@gmail.com>
2026-08-22 10:56:54 +0200

    Route P5 Aeon Match organizer page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import CloudWorkspace from './CloudWorkspace.jsx'
 import ProductWorkspace from './ProductWorkspace.jsx'
 import PublicDecksPage from './PublicDecksPage.jsx'
+import AeonMatchPage from './AeonMatchPage.jsx'
 import { PodMatchPage,SharedAnalysisPage } from './ProductPages.jsx'
 import './styles.css'
 import './readability.css'
@@ -13,12 +14,14 @@ const path=window.location.pathname
 const publicDeckRoute=path==='/decklists-publiques'||path.startsWith('/decklists-publiques/')
 const shareRoute=path.startsWith('/a/')
 const podRoute=path==='/pod'
+const matchRoute=path==='/match'
 const publicLabel=localStorage.getItem('aeon-lang')==='fr'?'Decklists publiques':'Public decks'
 
 let page
 if(publicDeckRoute)page=<PublicDecksPage/>
 else if(shareRoute)page=<SharedAnalysisPage/>
 else if(podRoute)page=<PodMatchPage/>
+else if(matchRoute)page=<AeonMatchPage/>
 else page=<><ProductWorkspace><CloudWorkspace><App /></CloudWorkspace></ProductWorkspace><a className="publicLibraryShortcut" href="/decklists-publiques"><span>{publicLabel}</span></a></>
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>{page}</React.StrictMode>)
