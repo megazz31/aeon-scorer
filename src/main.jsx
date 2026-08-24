87a07bc61688a70d2cafa4728d195b7f33ca7c0f
megazz31 <dandria.alan@gmail.com>
2026-08-24 09:55:18 +0200

    Route product workflows through clearer pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,27 @@ import App from './App.jsx'
 import CloudWorkspace from './CloudWorkspace.jsx'
 import ProductWorkspace from './ProductWorkspace.jsx'
 import PublicDecksPage from './PublicDecksPage.jsx'
-import AeonMatchPage from './AeonMatchPage.jsx'
-import { PodMatchPage,SharedAnalysisPage } from './ProductPages.jsx'
+import TournamentPage from './TournamentPage.jsx'
+import { CompareDecksPage,EventTablesPage,SharePage } from './ProductRouteShell.jsx'
 import './styles.css'
 import './readability.css'
 import './publicDecks.css'
+import './ux.css'
 
 const path=window.location.pathname
 const publicDeckRoute=path==='/decklists-publiques'||path.startsWith('/decklists-publiques/')
 const shareRoute=path.startsWith('/a/')
-const podRoute=path==='/pod'
-const matchRoute=path==='/match'
+const compareRoute=path==='/pod'
+const tablesRoute=path==='/match'
+const tournamentRoute=path==='/tournoi'
 const publicLabel=localStorage.getItem('aeon-lang')==='fr'?'Decklists publiques':'Public decks'
 
 let page
 if(publicDeckRoute)page=<PublicDecksPage/>
-else if(shareRoute)page=<SharedAnalysisPage/>
-else if(podRoute)page=<PodMatchPage/>
-else if(matchRoute)page=<AeonMatchPage/>
+else if(shareRoute)page=<SharePage/>
+else if(compareRoute)page=<CompareDecksPage/>
+else if(tablesRoute)page=<EventTablesPage/>
+else if(tournamentRoute)page=<TournamentPage/>
 else page=<><ProductWorkspace><CloudWorkspace><App /></CloudWorkspace></ProductWorkspace><a className="publicLibraryShortcut" href="/decklists-publiques"><span>{publicLabel}</span></a></>
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>{page}</React.StrictMode>)
