a092649ce5a67674b33a0332b7e7700e8057c6fd
megazz31 <dandria.alan@gmail.com>
2026-08-25 15:29:24 +0200

    chore(semantic15): bump semantic contract version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 export const AEON_VERSION='3.3.0'
 export const AEON_LABEL='v3.3'
 export const ENGINE_VERSION=AEON_VERSION
-export const SEMANTIC_VERSION='3.3.0-semantic-14'
+export const SEMANTIC_VERSION='3.3.0-semantic-15'
 export const MODEL_ID='sequence-access-v3.2-semantic'
